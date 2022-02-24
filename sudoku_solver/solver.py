03acfc98446756c4b7eae09eebf4917532f7b5c6
Jonas Meinertz Hansen <jonas@meinertz.org>
2022-02-24 21:27:57 +0100

    Implement solver function

---
+++ sudoku_solver/solver.py
@@ -0,0 +1,27 @@
+from sudoku_solver.identify_potential_digits import identify_potential_digits
+from sudoku_solver.reader import read_sudoku
+
+
+def solve(board):
+    updated_board = list(board)
+    for position in range(81):
+        if updated_board[position] != "0":
+            continue
+        potential_digits = identify_potential_digits(position, "".join(updated_board))
+        if len(potential_digits) == 1:
+            updated_board[position] = tuple(potential_digits)[0]
+
+    updated_board = "".join(updated_board)
+    if board == updated_board:
+        return updated_board
+    return solve(updated_board)
+
+
+if __name__ == "__main__":
+    num_tests = 10000
+    correct = 0
+    for i in range(num_tests):
+        puzzle, solution = read_sudoku(1)
+        correct += solve(puzzle) == solution
+
+    print(f"{correct} out of {num_tests} were solved correctly")
