29dbedc030db14a091aea3dc4e78b032b2e62bab
Jonas Meinertz Hansen <jonas@meinertz.org>
2022-02-24 19:36:00 +0100

    Simplify test parametrization

--- tests/test_identify_potential_digits.py
+++ tests/test_identify_potential_digits.py
@@ -11,26 +11,26 @@ board = (
 
 
 @pytest.mark.parametrize(
-    "row_index,_board,expected_digits",
+    "row_index,expected_digits",
     [
-        (0, board, ["7", "4", "3"]),
-        (1, board, ["4", "9", "6", "1"]),
-        (8, board, ["7", "4", "8", "2"]),
+        (0, ["7", "4", "3"]),
+        (1, ["4", "9", "6", "1"]),
+        (8, ["7", "4", "8", "2"]),
     ],
 )
 @pytest.mark.parametrize("column_index", list(range(9)))
-def test_get_digits_in_row(row_index, column_index, _board, expected_digits):
-    assert get_digits_in_row(row_index * 9 + column_index, _board) == expected_digits
+def test_get_digits_in_row(row_index, column_index, expected_digits):
+    assert get_digits_in_row(row_index * 9 + column_index, board) == expected_digits
 
 
 @pytest.mark.parametrize(
-    "column_index,_board,expected_digits",
+    "column_index,expected_digits",
     [
-        (0, board, ["8", "3", "9"]),
-        (1, board, ["7", "4", "9", "5", "8"]),
-        (8, board, ["3", "1", "5", "2"]),
+        (0, ["8", "3", "9"]),
+        (1, ["7", "4", "9", "5", "8"]),
+        (8, ["3", "1", "5", "2"]),
     ],
 )
 @pytest.mark.parametrize("row_index", list(range(9)))
-def test_get_digits_in_column(row_index, column_index, _board, expected_digits):
-    assert get_digits_in_column(row_index * 9 + column_index, _board) == expected_digits
+def test_get_digits_in_column(row_index, column_index, expected_digits):
+    assert get_digits_in_column(row_index * 9 + column_index, board) == expected_digits
