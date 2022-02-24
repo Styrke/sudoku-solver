844cbd32331252aa097e4e29e01541900d893d1f
Jonas Meinertz Hansen <jonas@meinertz.org>
2022-02-24 19:37:54 +0100

    Add function to get digits in same sector as position

--- sudoku_solver/identify_potential_digits.py
+++ sudoku_solver/identify_potential_digits.py
@@ -13,3 +13,17 @@ def get_digits_in_column(position: int, board: str) -> List[str]:
     column_number = position % 9
     column = board[column_number::9]
     return list(column.replace("0", ""))
+
+
+def get_digits_in_sector(position: int, board: str) -> List[str]:
+    """Get all the digits that are in the same sector as the given position"""
+    sector_row = position // 27
+    sector_column = position % 9 // 3
+    rows = range(3 * sector_row, 3 * sector_row + 3)
+    cols = range(3 * sector_column, 3 * sector_column + 3)
+    return [
+        board[row * 9 + col]
+        for row in rows
+        for col in cols
+        if board[row * 9 + col] != "0"
+    ]
--- tests/test_identify_potential_digits.py
+++ tests/test_identify_potential_digits.py
@@ -3,6 +3,7 @@ import pytest
 from sudoku_solver.identify_potential_digits import (
     get_digits_in_row,
     get_digits_in_column,
+    get_digits_in_sector,
 )
 
 board = (
@@ -34,3 +35,19 @@ def test_get_digits_in_row(row_index, column_index, expected_digits):
 @pytest.mark.parametrize("row_index", list(range(9)))
 def test_get_digits_in_column(row_index, column_index, expected_digits):
     assert get_digits_in_column(row_index * 9 + column_index, board) == expected_digits
+
+
+@pytest.mark.parametrize(
+    "position,expected_digits",
+    [(position, ["7", "4", "8"]) for position in [0, 1, 2, 9, 10, 11, 18, 19, 20]]
+    + [
+        (position, ["9", "6", "3", "4"])
+        for position in [3, 4, 5, 12, 13, 14, 21, 22, 23]
+    ]
+    + [
+        (position, ["9", "1", "5", "8", "2"])
+        for position in [60, 61, 62, 69, 70, 71, 78, 79, 80]
+    ],
+)
+def test_get_digits_in_sector(position, expected_digits):
+    assert get_digits_in_sector(position, board) == expected_digits
